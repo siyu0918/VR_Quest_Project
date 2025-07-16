d00624df270c6db77d0bf078851144df99782fb9
SiyuCHENG <chengsiyu52@gmail.com>
2025-07-17 02:25:18 +0800

    Initial commit of VR Quest Project

---
+++ Assets/DanceController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceController : MonoBehaviour
+{
+    public Animator animator;
+
+    public void PlayDance001()
+    {
+        animator.SetTrigger("Play001");
+    }
+
+    public void PlayDance002()
+    {
+        animator.SetTrigger("Play002");
+    }
+
+    public void PlayDance003()
+    {
+        animator.SetTrigger("Play003");
+    }
+
+    public void PlayDance004()
+    {
+        animator.SetTrigger("Play004");
+    }
+
+    public void PlayDance005()
+    {
+        animator.SetTrigger("Play005");
+    }
+
+    public void PlayDance006()
+    {
+        animator.SetTrigger("Play006");
+    }
+
+    public void PlayDance007()
+    {
+        animator.SetTrigger("Play007");
+    }
+
+    public void PlayDance008()
+    {
+        animator.SetTrigger("Play008");
+    }
+
+    public void PlayDance009()
+    {
+        animator.SetTrigger("Play009");
+    }
+}
